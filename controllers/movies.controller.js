16441de895551767118bf7f9a0cd1a9638e6468f
Ayesha <ayeshaayesha16.2003@gmail.com>
2025-02-12 17:51:00 +0530

    Update functionality

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,7 +1,12 @@
 import Movie from "../models/movies.model.js";
 
-export const MovieIndex=(req,res)=>{
-    res.send("Get all movie lists");
+export const MovieIndex=async(req,res)=>{
+   try{
+    const movie=await Movie.find()
+    res.json(movie)
+   }catch(error){
+    res.status(500).json({message:error.message});
+   }
 };
 
 export const MovieCreate=async(req,res)=>{
@@ -12,6 +17,7 @@ export const MovieCreate=async(req,res)=>{
         title:req.body.title,
         desc:req.body.desc
     });
+
 try{
     const movie=await newMovie.save();
     return res.status(201).json(movie);
@@ -22,8 +28,44 @@ try{
 
 };
 
-export const MovieUpdate=(req,res)=>{
-    res.send("Updata a movie");
+export const MovieDetails=async(req,res)=>{
+    try{
+        const movie= await Movie.findById(req.params.id);
+        if(movie==null){
+            return res.status(404).json({message:"Cannot find movie"});
+        }else{
+            res.json(movie)
+        }
+       }catch(error){
+         return res.status(500).json({message:error.message});
+       }
+}
+
+export const MovieUpdate=async(req,res)=>{
+    try{
+        const result= await Movie.findByIdAndUpdate({_id:req.params.id},{
+        title:req.body.title,
+        desc:req.body.desc
+    },{
+        new:true,
+    });
+    res.status(200).json(result);
+}catch(error){
+    res.status(400).json({message:error.message});
+}
+   //Validate the user input
+//    if(req.body.title!=null){
+//     res.movie.title=req.body.title;
+//    }
+//    if(req.body.desc!=null){
+//     res.movie.desc=req.body.desc;
+//    }
+//    try{
+//     const updateMovie=await res.movie.save();
+//     res.json(updateMovie);
+//    }catch(error){
+//     res.status(400).json({message:error.message});
+//    }
 };
 
 export const MovieDelete=(req,res)=>{
