091367694327540e31fbb1dd6ee5640b41fcfc4b
Ayesha <ayeshaayesha16.2003@gmail.com>
2025-02-12 16:56:15 +0530

    add models

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,13 +1,25 @@
+import Movie from "../models/movies.model.js";
+
 export const MovieIndex=(req,res)=>{
     res.send("Get all movie lists");
 };
 
-export const MovieCreate=(req,res)=>{
+export const MovieCreate=async(req,res)=>{
     //id,title,desc
-    //create the movie info
-    console.log(req.body);
-    // res.send("Create a movie");
-    return res.json(req.body);
+   
+    //Validate the user data
+    const newMovie=new Movie({
+        title:req.body.title,
+        desc:req.body.desc
+    });
+try{
+    const movie=await newMovie.save();
+    return res.status(201).json(movie);
+}catch(error){
+    return res.status(400).json({message:error.message});
+}
+
+
 };
 
 export const MovieUpdate=(req,res)=>{
