8102b1083adf69abe778ea11408bca3e5deb3074
Ayesha <ayeshaayesha16.2003@gmail.com>
2025-02-12 17:51:12 +0530

    Update functionality

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { MovieCreate, MovieDelete, MovieIndex, MovieUpdate } from "../controllers/movies.controller.js";
+import { MovieCreate, MovieDelete, MovieDetails, MovieIndex, MovieUpdate } from "../controllers/movies.controller.js";
 
 const router=express.Router();
 
 router.get('/',MovieIndex);
+
+router.get('/:id',MovieDetails);
 
 router.post('/',MovieCreate);
 
