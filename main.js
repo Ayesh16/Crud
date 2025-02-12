de6dba52a62dde02a649bf66fa4890e7fcb7e34b
Ayesha <ayeshaayesha16.2003@gmail.com>
2025-02-12 15:17:49 +0530

    route proper format with npm commands

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const express = require('express')
+import express from "express";
+import movieRoutes from"./routes/movies.route.js"
 const app = express()
 const port = 3000
 
@@ -7,23 +8,7 @@ app.get('/', (req, res) => {
 })
 
 //Crud Functionalities
-
-app.get('/movies',()=>{
-
-})
-
-app.post('/movies',()=>{
-
-})
-
-app.put('/movies/:id',()=>{
-
-})
-
-app.delete('/movies/:id',()=>{
-
-})
-
+app.use('/movies',movieRoutes);
 app.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}`)
 })
